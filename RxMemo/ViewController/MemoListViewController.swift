f4056e1daa6612151d88b0daf2ed128cb3c08885
DoheeKim <allie0147@gmail.com>
2021-06-20 22:31:13 +0900

    Feat: add VC.swift

---
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by 김도희 on 2021/06/20.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
